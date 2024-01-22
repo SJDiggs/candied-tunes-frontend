bbfa68d5ed256d34d24cccdc6688aacaaafcb3a1
stevediggs70 <stevediggs670@gmail.com>
2024-01-21 21:05:08 -0500

    readme, minor styling enhancements, code optimization

--- README.md
+++ README.md
@@ -1,8 +1,65 @@
-# React + Vite
+# Welcome the the Candied Tunes web app!  
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+The Candied Tunes app was conceived by Musicians, Fans and Technologists as a way for fans to digitally interact with Musicians while they are performing at a show, by requesting songs, sending tips, signing up for emails and much more.  
 
-Currently, two official plugins are available:
+There are two persona's, 1: The Audience, and 2: The Musician. 
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Audience users can:
+- View the Musicians Playlist
+- Request songs
+- Tip the artist
+- View upcoming shows
+- Learn more about the band and.or performing artist bio
+- Sign up for emails
+- ...and much more!  
+
+Musician's can interactively view requested songs, tip amounts, while gigging, and perform a variety of Management features, such as:
+- Song Management:
+    - View song requests and tip amounts from the Audience
+    - Reset the Playlist after the show ends
+    - Add new songs to the playlist
+    - Remove songs from the playlist
+    - Update a songs information such as the main instrument, song name or artist name
+- Show Management:
+    - Add a show to your list of shows
+    - Remove a show 
+    - Update a shows information such as the date, location, time and notes
+- Admin:
+    - Update the bands or artist Bio
+    - Create and send fans an email
+
+There is also an Auth0 login feature which is used to identify Musicians and sign-up Audience users so they can receive emails, etc
+
+
+# Production Link:
+
+- [here](https://netlify link)
+
+
+# Technologies Used:
+
+- Javascript
+- Tailwind CSS
+- HTML
+- Node JS
+- Express JS
+- React JS
+- MongoDB
+- HTML / CSS
+- Auth0
+- Vite
+
+
+# Acknowledgments/References/Source Material
+
+- Contributing Artist:  Candie Tremblay
+
+
+# Future Enhancements/Releases:
+
+- Authentication updates
+- Bug fixes
+- Shows features
+- Tip system/ payment gateway integration
+- About/Bio
+- Email feature
--- src/pages/Requests.jsx
+++ src/pages/Requests.jsx
@@ -4,6 +4,18 @@ import { useRequestContext } from "../components/Context/RequestContext";
 const Requests = () => {
   const { requestedSongs } = useRequestContext();
 
+  const handlePlayClick = async (songId) => {
+    try {
+      
+      console.log('Requests - Play button clicked')
+      // Logic to handle the Play button click
+        // update the songPlayed to true on the songs table
+        // reset the state of the landing table so that the request button is disabled and the song is grayed out
+    } catch (err) {
+      console.log('Requests - Play Button error:', err.message);
+    }
+  };
+
   return (
       <section className="song-list flex justify-center h-screen">
           <div className="overflow-x-auto">
@@ -14,6 +26,7 @@ const Requests = () => {
             <th className="py-3 px-6 border-b border-stone-600">Artist</th>
             <th className="py-3 px-6 border-b border-stone-600">Instrument</th>
             <th className="py-3 px-6 border-b border-stone-600">Requesters Name</th>
+            <th className="py-3 px-6 border-b border-stone-600">Tip Amount</th>
             <th className="py-3 px-6 border-b border-stone-600">Play</th>
           </tr>
         </thead>
@@ -23,11 +36,13 @@ const Requests = () => {
               <td className="py-4 px-6">{song.songName}</td>
               <td className="py-4 px-6">{song.songArtist}</td>
               <td className="py-4 px-6">{song.songInstrument}</td>
-              {/* Assuming you have a requester's name in your song object */}
-              <td className="py-4 px-6">{song.requestersName}</td>
-              <td className="px-6 py-1 rounded-full bg-purple-900 text-white">
+              {/* Future feature to get the requester's name from login or allowing user to enter at landing page */}
+              <td className="py-4 px-6">Joey Person</td>
+              {/* Future feature when tip system implemented */}
+              <td className="py-4 px-6">$10.00</td>
+              <td className="px-4 py-4 rounded bg-purple-900 text-white">
                 {/* Add your Play button logic here */}
-                <button onClick={() => handlePlayClick(song)}>Play</button>
+                <button onClick={() => handlePlayClick(song.songId)}>Play</button>
               </td>
             </tr>
           ))}
